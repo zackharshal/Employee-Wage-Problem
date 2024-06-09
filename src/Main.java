a4a58ee16fd2ac790b1093ace91b8c27d410b623
zackharshal <harshalgotarne25@gmail.com>
2024-06-09 20:57:25 +0530

    Part time employee
    Added a part time employee and his daily wage

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation problem.");
-        int IS_FULL_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int check = (int) ((Math.random() * 10) % 2);
+        int check = (int) ((Math.random() * 10) % 3);
         if (check == IS_FULL_TIME){
             empHrs = 8;
+        }else if(check == IS_PART_TIME){
+            empHrs = 4;
         }else{
             empHrs = 0;
         }
